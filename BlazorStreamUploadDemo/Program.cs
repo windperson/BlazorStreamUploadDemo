40e3503695ad957b7816f3e7f093d386809e11e2
Chen-Yu Pao <windperson@gmail.com>
2021-01-12 19:17:05 +0800

    Add generator example

--- BlazorStreamUploadDemo/Program.cs
+++ BlazorStreamUploadDemo/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace BlazorStreamUploadDemo
 {
@@ -13,11 +14,26 @@ namespace BlazorStreamUploadDemo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                Console.WriteLine(ex);
+                throw;
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
---
+++ BlazorStreamUploadDemo/wwwroot/frontSender.js
@@ -0,0 +1,15 @@
+﻿function* generateStreamCharGenerator(srcStrng) {
+    for (let i = 0; i < srcStrng.length; i++) {
+        let ret = srcStrng.charAt(i);
+        if (i === srcStrng.length - 1) {
+            return ret;
+        }
+        yield ret;
+    }
+}
+
+// noinspection JSUnusedGlobalSymbols
+export function createStreamGenerator(inputId) {
+    let source = document.getElementById(inputId).value;
+    return generateStreamCharGenerator(source);
+}
